803ae891aa66ec596666b868c02b8d0456a02a05
turtletennis <stephenjenkins42@gmail.com>
2022-02-12 23:17:13 +0000

    tag stone prefabs with "rock" add variables for movement speed, health and hit damage. Get collision with rocks working and subtracting health. Reset game if you take too much damage and report how long you lasted.

--- gently down the stream/Assets/scripts/BoatController.cs
+++ gently down the stream/Assets/scripts/BoatController.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class BoatController : MonoBehaviour
 {
-    public InputAction fireAction;
-    PlayerInput playerInput;
+    
+    
     private Vector2 mousePosition;
+    [Header("Movement")]
+    public Transform startPosition;
+    public float startSpeed;
+    public float acceleration;
+    [Range(-100.0f, 100.0f)]
+    public float steeringAngleChange;
+    public float steeringPower;
+    private float currentSpeed;
+    [Space(10)]
+    
+    [Header("Health and Damage")]
+    public float totalHealth;
+    public float minRockHitDamage;
+    public float rockDamageMultiplier;
+    public float maxRockHitDamage;
+    private float currentHealth;
+    private System.Diagnostics.Stopwatch lifetime;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
-        if(playerInput==null)
-        {
-            Debug.LogError("Failed to find player input component");
-        }
+        Reset();
+    }
+
+    private void Reset()
+    {
+        lifetime = System.Diagnostics.Stopwatch.StartNew();
+        this.transform.SetPositionAndRotation(startPosition.position,startPosition.rotation);
+        currentSpeed = startSpeed;
+        currentHealth = totalHealth;
     }
 
     private void Awake()
@@ -23,15 +47,58 @@ public class BoatController : MonoBehaviour
         
     }
 
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if (col.gameObject.tag == "rock")
+        {
+            
+            float damage = col.relativeVelocity.magnitude * rockDamageMultiplier;
+            if(damage < minRockHitDamage)
+            {
+                damage = minRockHitDamage;
+            }
+            else if(damage > maxRockHitDamage)
+            {
+                damage = maxRockHitDamage;
+            }
+            currentHealth -= damage;
+            Debug.Log($"Ouch a rock! took {damage} damage. Health {currentHealth}/{totalHealth}");
+        }
+
+        if(currentHealth<=0)
+        {
+            Die();
+        }
+        
+    }
+
+    void Die()
+    {
+        lifetime.Stop();
+        Debug.Log($"You lasted {lifetime.Elapsed.TotalSeconds} seconds");
+        Reset();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        currentSpeed += acceleration * Time.deltaTime;
         //mousePosition = Mouse.current.position.ReadValue();
-
-        transform.Translate(currentDirection * Time.deltaTime);
-
+        var velocity = currentDirection;
+        velocity.y += currentSpeed;
+        transform.Translate(velocity * Time.deltaTime);
+        if(velocity.x!=0)
+        {
+            Vector3 rotation = new Vector3(0, 0, -velocity.x * steeringAngleChange);
+            transform.Rotate(rotation * Time.deltaTime);
+        }
     }
-    Vector3 currentDirection;
+
+
+
+    Vector2 currentDirection;
     public void OnMove(InputValue value)
     {
         currentDirection = value.Get<Vector2>();
--- gently down the stream/Assets/scripts/CameraFollow.cs
+++ gently down the stream/Assets/scripts/CameraFollow.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public Transform followTransform;
-    
-    void FixedUpdate()
+    public BoxCollider2D mapBounds;
+
+    private float xMin, xMax, yMin, yMax;
+    private float camY, camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+
+    private void Start()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
-
-
+        xMin = mapBounds.bounds.min.x;
+        xMax = mapBounds.bounds.max.x;
+        yMin = mapBounds.bounds.min.y;
+        yMax = mapBounds.bounds.max.y;
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
+        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
+
+
     }
 }
